3b8a52f7a63d5cb0d3e4f7b3d03cb5b7bec8d6e5
Diana Shafarenko <shafarenko.di@gmail.com>
2024-03-25 20:24:29 +0300

    accordion

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,10 +18,17 @@ const swiperHero = new Swiper('.hero__swiper', {
 });
 
 const swiperAbout = new Swiper(".about__slider", {
-  slidesPerView: 3,
+  slidesPerView: 4,
+  spaceBetween: 20,
   freeMode: true,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+});
+
+document.querySelectorAll('.accordion__triger').forEach((item) => {
+  item.addEventListener('click', () => {
+    item.parentNode.classList.toggle('accordion__item--active')
+  })
 });
